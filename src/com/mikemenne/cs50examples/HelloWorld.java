f62429e61808b92f589b89df60405dba7ba5c64b
MoMenne <mpmenne@electronicsystemssupport.com>
2014-02-28 23:26:37 -0600

    first commit

---
+++ src/com/mikemenne/cs50examples/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mikemenne.cs50examples;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mpmenne
+ * Date: 2/28/14
+ * Time: 10:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+}
