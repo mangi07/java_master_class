d9a4a30ddf3b03211d425360d59b505c75b6e267
mangi07 <benrolson@gmail.com>
2015-12-14 20:27:41 -0700

    pset1 command line java

--- src/JavaCommandLine.java
+++ src/JavaCommandLine.java
@@ -1,16 +1,19 @@
-import java.io.Console;
 
+
+import java.io.Console;
 
 public class JavaCommandLine {
 
     public static void main(String[] varArgs) {
-        System.out.println("Hello I am a computer");
+	System.out.println("Hello I am a computer");
+	
+	Console c = System.console();
+	String name = c.readLine("What is your name?? ");
+	System.out.println("Hello " + name);
 
-        Console c = System.console();
-        String name = c.readLine("What is your name?? ");
-        System.out.println("Hello " + name);
 
 
     }
+
 
 }
